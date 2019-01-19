15d80251160c0103afc89063fb45d98591a8ead5
saquibw <saquib.waheed@yahoo.com>
2019-01-19 21:40:26 +0600

    Understanding components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `<h1>Hey guys</h1>
+  <p>{{myObject.location}}</p>
+
+  <ul>
+    <li *ngIf="myArr;then tmpl1 else tmpl2"></li> 
+  </ul>
+
+  <ng-template #tmpl1>Yes, I do.</ng-template>
+  <ng-template #tmpl2>No, I don't.</ng-template>
+  `,
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'angular-playground';
+  myArr = "a";
 }
