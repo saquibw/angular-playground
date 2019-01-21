ff57545de0015da8f8ab46b3e307eaa157186daf
saquibw <saquib.waheed@yahoo.com>
2019-01-21 08:11:31 +0600

    Property binding implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,18 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `<h1>Hey guys</h1>
-  <p>{{myObject.location}}</p>
 
-  <ul>
-    <li *ngIf="myArr;then tmpl1 else tmpl2"></li> 
-  </ul>
-
-  <ng-template #tmpl1>Yes, I do.</ng-template>
-  <ng-template #tmpl2>No, I don't.</ng-template>
+  <button [disabled]="buttonStatus">My Button</button>
   `,
   styleUrls: ['./app.component.css']
 })
 
 export class AppComponent {
-  myArr = "a";
+  buttonStatus = false;
 }
